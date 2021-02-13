c608c603a833d4627965e0bf4803715454bafe49
jazmand <atothezmand@gmail.com>
2021-02-14 03:14:34 +0900

    first commit

---
+++ gairaigo/catalog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+import pandas as pd 
+import random
+
+# Create your views here.
+def generate(request):
+    df = pd.read_excel (r'C:\Users\Judi\Desktop\Programming\Katakana Words (Gairaigo)\GairaigoExcel.xlsx')
+
+    x=[]
+    for value in  df.iloc[:, 1]:
+        x.append(value)
+    jp = random.choice(x)
+    index = x.index(jp)
+
+    y=[]
+    for value in  df.iloc[:, 2]:
+        y.append(value)
+    eng = y[index]    
+
+    return render(request, "catalog/layout.html", {
+        "jp": jp,
+        "eng": eng        
+    })
