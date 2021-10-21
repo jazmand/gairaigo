b6ae358aaa858713cff375d4effc0a07fa748ae8
jazmand <atothezmand@gmail.com>
2021-10-21 16:30:42 +0900

    update excel file relative path

--- gairaigo/catalog/views.py
+++ gairaigo/catalog/views.py
@@ -5,7 +5,7 @@ import random
 
 # Create your views here.
 def generate(request):
-    df = pd.read_excel (r'/gairaigo/GairaigoExcel.xlsx')
+    df = pd.read_excel ("../GairaigoExcel.xlsx")
 
     x=[]
     for value in  df.iloc[:, 0]:
